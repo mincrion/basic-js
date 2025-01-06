f178fc6fd49d04d15118f4c4a451f533d9e18d18
mincrion <mincrion@google.com>
2025-01-07 00:52:28 +0400

    feat: the functionality of the VigenereCipheringMachine has been implemented

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this._enigma(message, key, 'encrypt');
   }
+  decrypt(message, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this._enigma(encryptedMessage, key, 'decrypt');
+  }
+  _enigma(input, key, method) {
+    const A = 65;
+    const Z = 90;
+    const ALPHABET_SIZE = 26;
+
+    const inputUpper = input.toUpperCase();
+    const keyUpper = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < inputUpper.length; i++) {
+      const charCode = inputUpper.charCodeAt(i);
+      if (charCode >= A && charCode <= Z) {
+        const shift = keyUpper.charCodeAt(keyIndex % keyUpper.length) - A;
+        const newCharCode =
+          method === 'encrypt'
+            ? ((charCode - A + shift) % ALPHABET_SIZE) + A
+            : ((charCode - A - shift + ALPHABET_SIZE) % ALPHABET_SIZE) + A;
+        result += String.fromCharCode(newCharCode);
+        keyIndex ++;
+      } else {
+        result += inputUpper[i];
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
 }
 
 module.exports = {
