e41bf68ecee09b05993b9121dd2b911ed72d24ff
mincrion <mincrion@google.com>
2025-01-07 01:15:51 +0400

    feat: the functionality of the getDNSStats has been implemented

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let subDomain = '';
+    for (const part of parts) {
+      subDomain = subDomain ? `${subDomain}.${part}` : `.${part}`;
+      result[subDomain] = (result[subDomain] || 0) + 1;
+    }
+  }
+  return result;
 }
 
 module.exports = {
